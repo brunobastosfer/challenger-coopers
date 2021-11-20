91b6b93e02334a60566b3beaf3dd21d81d9d5e96
brunobastosfer <brunobastos@edu.unifor.br>
2021-11-19 23:01:15 -0300

    criando uma nova todo, renderizando em tela e salvando no banco

--- backend/src/modules/todos/useCases/createTodo/CreateTodoUseCase.ts
+++ backend/src/modules/todos/useCases/createTodo/CreateTodoUseCase.ts
@@ -11,11 +11,6 @@ class CreateTodoUseCase {
   constructor(private todosRepository: ITodosRepository){}
 
   async execute({ nome, concluida, token }: IRequest): Promise<void> {
-    const todoAlreadyExists = await this.todosRepository.findByName(nome)
-
-    if (todoAlreadyExists) {
-      throw new Error("Já existe uma todo com esse nome")
-    }
 
     return this.todosRepository.create({ nome, concluida, token })
   }
